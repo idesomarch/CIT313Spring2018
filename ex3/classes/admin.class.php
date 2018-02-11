f7e350e4f30ae6c0fcce2b86a949b8817c94f525
idesovmarch <bdhammer2288@gmail.com>
2018-02-11 17:15:06 -0500

    Clone ex2-to-ex3

---
+++ ex3/classes/admin.class.php
@@ -0,0 +1,21 @@
+<?php
+class admin extends user {
+
+public function __construct() {
+  parent::__construct();
+
+}
+public function __set($name,$value) {
+  $this->$name = $value;
+  return;
+}
+public function __get($name) {
+return $this->$name;
+}
+public function __destruct() {
+
+}
+
+}
+
+ ?>

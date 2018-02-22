8ed6f619946b9abeaae7b72767f9d03077f57781
idesovmarch <30424247+idesovmarch@users.noreply.github.com>
2018-02-22 16:38:55 -0500

    router back to beginning

--- a2/application/router.php
+++ a2/application/router.php
@@ -1,18 +1,12 @@
 <?php
 include '_includes/config.php'
-    //require_once 'application/load.class.php';
-    //require_once 'application/model.class.php';
-    //require_once 'application/controller.class.php';
-  //  function myLoad($class) {
-    //  include_once('application/controllers/'.strtolower($class).'.class.php');
-    //  include_once('application/models/'.strtolower($class).'.class.php');
-    //  include_once('application/'.strtolower($class).'.class.php');
+    
+    function myLoad($class) {
+     include_once('application/controllers/'.strtolower($class).'.class.php');
+     include_once('application/models/'.strtolower($class).'.class.php');
+     include_once('application/'.strtolower($class).'.class.php');
 
-function __autoload($class_name) {
-        if (file_exists('application/controllers' . $class_name . '.class.php')) {
-            require_once ($directory . $class_name . '.class.php');
-            return;
-        }
+
     }
 }
 spl_autoload_register('myLoad');
@@ -20,3 +14,10 @@ spl_autoload_register('myLoad');
     new Controller();
 
 ?>
+
+
+
+
+//require_once 'application/load.class.php';
+    //require_once 'application/model.class.php';
+    //require_once 'application/controller.class.php';

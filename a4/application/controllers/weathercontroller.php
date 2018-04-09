9ce6bc269d9af9b367ae1c44e0bd2e58018b4aca
idesovmarch <bdhammer2288@gmail.com>
2018-04-08 21:30:45 -0400

    a

--- a4/application/controllers/weathercontroller.php
+++ a4/application/controllers/weathercontroller.php
@@ -10,7 +10,7 @@ class WeatherController extends Controller{
 	}
 
 	public function getresults() {
-		$xml = simplexml_load_file("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=2bd21f7d5ed04685a12170423180804&q=".$_POST['zip']."&format=xml&num_of_days=5");
+		$xml = simplexml_load_file("http://api.wunderground.com/api/9275ef0b6f8bd4ea/conditions/q/" .$_POST['zip'].".xml");
 		$this->set(result,true);
 		$this->set(weather,$xml);
 	}
--- a4/views/home/index.php
+++ a4/views/home/index.php
@@ -1,12 +1,13 @@
 <?php
 require_once('application/config.php');
 include('views/elements/header.php');?>
+
 <div class="container">
 	<div class="page-header">
     <h1>Latest News From <?php echo $rss_title;?></h1>
-		
-  </div>
 
+  </div>
+	
 		<h4><a href="<?php echo $link;?>"><?php echo $item;?></a> (<?php echo $pubDate;?>)</h4>
 		<p><?php echo $description ?></p>
 
